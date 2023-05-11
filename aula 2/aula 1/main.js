23838342b0d39359ad19a5763c067010cee23ba9
DAMASCENO <luambito93@gmail.com>
2023-05-11 19:20:04 -0300

    Html

---
+++ aula 2/aula 1/main.js
@@ -0,0 +1,6 @@
+let a = 11;
+if ( a > 10){
+    console.log("é maior que 10");
+}else{
+    console.log("é menor que 10")
+}
--- aula1/aula 20/index.js
+++ aula1/aula 20/index.js
@@ -1,1 +1,4 @@
-                 
+const usu = 'Gabriel';
+const senha = '123456';
+const vai = usu === 'Gabriel' && senha === '123456';
+console.log(vai);   
